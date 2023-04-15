4002806b211f7f4a51e911fcd88126c8dc174311
juliandurelli <durellijulian@gmail.com>
2023-04-15 01:02:40 -0300

    termine el formulario CV

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ function validacion()
       Array.from(forms).forEach(form => {
         if (!form.checkValidity()) {
           form.classList.add('was-validated')
+        }else
+        {
+          const toastLiveExample = document.getElementById('liveToast');
+          const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+          toastBootstrap.show()
+          form.classList.remove('was-validated')
+          document.getElementById("formulario").reset();
         }
       })
     }
